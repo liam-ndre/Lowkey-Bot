48bd11189ebec910a872dfbac55c3bd152727ccf
Liam <liameguia@gmail.com>
2022-06-25 17:14:21 +0100

    fixed interaction reply problem

--- src/events/interactionCreate2.js
+++ src/events/interactionCreate2.js
@@ -15,5 +15,19 @@ module.exports = async (client, interaction) => {
         const role = interaction.guild.roles.cache.get(v.role);
 
         if ((v.emoji !== emoji.name && v.emoji !== emoji.id)) return;
+
+        if (!member.roles.cache.has(role.id)) {
+            member.roles.add(role).then(() => {
+                interaction.reply({ content: `I gave you the **${role.name}** role in ${interaction.guild.name}`, ephemeral: true })
+            }).catch(() => {
+                interaction.reply({ content: `I was unable to give you the role in ${interaction.guild.name}`, ephemeral: true })
+            })
+        } else {
+            member.roles.remove(role).then(() => {
+                interaction.reply({ content: `I removed the **${role.name}** role from you in ${interaction.guild.name}`, ephemeral: true })
+            }).catch(() => {
+                interaction.reply({ content: `I was unable to remove a role from you in ${interaction.guild.name}`, ephemeral: true })
+            })
+        }
     })
 }
