3096491cda2f89e9b1534902a27478e914ce22f4
Liam <liameguia@gmail.com>
2022-06-26 11:57:29 +0100

    testing individual buttons

--- src/commands/general/button.js
+++ src/commands/general/button.js
@@ -9,9 +9,31 @@ module.exports = {
 
     run: async (client, interaction) => {
         const row = new MessageActionRow().addComponents(
-            new MessageButton().setStyle("LINK").setURL("https://google.com").setEmoji("🔍"),
+            new MessageButton().setStyle("LINK").setURL("https://google.com").setLabel("Google").setEmoji("🔍").setDisabled(true),
+            new MessageButton().setStyle("DANGER").setLabel("Danger Zone").setCustomId("1"),
+            new MessageButton().setStyle("PRIMARY").setLabel("Primary").setCustomId("2"),
+            new MessageButton().setStyle("SECONDARY").setLabel("Secondary").setCustomId("3"),
+            new MessageButton().setStyle("SUCCESS").setLabel("Success").setCustomId("4"),
         );
 
-        interaction.reply({ components: [row], content:"Buttons!" });
+        await interaction.reply({ components: [row], content: "Buttons!" });
+        const msg = await interaction.fetchReply();
+
+        const collector = msg.createMessageComponentCollector({ filter: (i) => i.user.id === interaction.user.id && parseInt(i.customId), time: 5000 })
+
+        collector.on('collect', (int) => collector.stop(int.customId));
+
+        collector.on('end', (ints, reason) => {
+            let content = "";
+
+            if (reason === "time") content = "You are way too slow to click on the buttons, bruh";
+            else if (reason === "1") content = "You clicked on the danger button";
+            else if (reason === "2") content = "You clicked on the secondary button";
+            else if (reason === "3") content = "You clicked on the primary button";
+            else if (reason === "4") content = "You clicked on the success button";
+            else content = "You click on a unknown button";
+
+            interaction.editReply({ components: [], content });
+        })
     }
 }
