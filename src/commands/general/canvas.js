5522506fa0f1339a7f09ddfcdb65198a52153100
Liam <liameguia@gmail.com>
2022-07-21 12:15:53 +0100

    rounded the avatar image

--- src/commands/general/canvas.js
+++ src/commands/general/canvas.js
@@ -11,7 +11,7 @@ module.exports = {
         const canvas = createCanvas(1200, 700);
         const ctx = canvas.getContext('2d');
         const avatar = await loadImage(interaction.user.displayAvatarURL({ dynamic: false, format: "png" }));
-        const diameter = 300;
+        const diameter = 500;
 
         ctx.fillStyle = "white";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -20,6 +20,11 @@ module.exports = {
         ctx.font = "bold 50px Arial"
         ctx.textAlign = "center";
         ctx.fillText("Filled text", 600, 670);
+
+        ctx.beginPath();
+        ctx.arc(canvas.width / 2, canvas.height / 2, diameter / 2, 0, 2 * Math.PI);
+        ctx.closePath();
+        ctx.clip();
 
         ctx.drawImage(avatar, canvas.width /  2 - diameter / 2, canvas.height / 2 - diameter / 2, diameter, diameter);
 
