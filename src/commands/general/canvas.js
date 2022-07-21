5ce7f869c167a7aec431e255d0b2ee2cbb383013
Liam <liameguia@gmail.com>
2022-07-21 12:09:33 +0100

    added avatars

--- src/commands/general/canvas.js
+++ src/commands/general/canvas.js
@@ -10,16 +10,18 @@ module.exports = {
     run: async (client, interaction) => {
         const canvas = createCanvas(1200, 700);
         const ctx = canvas.getContext('2d');
+        const avatar = await loadImage(interaction.user.displayAvatarURL({ dynamic: false, format: "png" }));
+        const diameter = 300;
 
         ctx.fillStyle = "white";
         ctx.fillRect(0, 0, canvas.width, canvas.height);
 
         ctx.fillStyle = "black";
+        ctx.font = "bold 50px Arial"
+        ctx.textAlign = "center";
+        ctx.fillText("Filled text", 600, 670);
 
-        ctx.strokeText("Stroked text", 600, 450, 80);
-        
-        ctx.textAlign = "center";	
-        ctx.fillText("Filled text", 600, 400);
+        ctx.drawImage(avatar, canvas.width /  2 - diameter / 2, canvas.height / 2 - diameter / 2, diameter, diameter);
 
         const image = new MessageAttachment(canvas.toBuffer(), "image.png");
         interaction.reply({ files: [image] });
